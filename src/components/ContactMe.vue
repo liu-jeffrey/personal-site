ea5fafd3c3daffc9381cda57b273ed9f3636373d
Jeffelini <jeffreyliu0117@gmail.com>
2019-09-11 19:41:54 -0400

    add contact section

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -3,26 +3,32 @@
     <h3>Let's get in touch</h3>
     <div class="links">
       <a href="mailto: jeffrey.liu@uwaterloo.ca">
-        <img src="../assets/email_logo.png" height="50" width="50">
+        <img src="../assets/email_logo.png" height="50" width="50" />
       </a>
       <a href="https://www.linkedin.com/in/liu-jeffrey/" target="_blank">
-        <img src="../assets/linkedin_logo.png" height="50" width="50">
+        <img src="../assets/linkedin_logo.png" height="50" width="50" />
       </a>
       <a href="http://github.com/liu-jeffrey" target="_blank">
-        <img src="../assets/github_logo.png" height="50" width="50">
+        <img src="../assets/github_logo.png" height="50" width="50" />
       </a>
     </div>
+    <p>Toronto, ON</p>
   </div>
 </template>
 
 
 <style scoped lang="scss">
 .contact {
-  height: 50vh;
+  height: 30vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  p {
+    text-align: center;
+    font-weight: 600;
+  }
 }
 
 .links {
@@ -33,6 +39,10 @@
 }
 
 a {
-  margin: 0 2vh;
+  margin: 0 3vh;
+
+  &:hover {
+    opacity: 0.7;
+  }
 }
 </style>
